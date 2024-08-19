d7de6bba66f63eb5355d22d2f3f03c3be9fd9d05
ma3laa <142917357+ma3laa@users.noreply.github.com>
2024-08-19 22:59:23 +0100

    loops js commit

---
+++ codecademy/doWhileStatement.js
@@ -0,0 +1,12 @@
+const firstMessage = 'I will print!';
+const secondMessage = 'I will not print!'; 
+
+// A do while with a stopping condition that evaluates to false
+do {
+ console.log(firstMessage)
+} while (true === false);
+
+// A while loop with a stopping condition that evaluates to false
+while (true === false){
+  console.log(secondMessage)
+};
