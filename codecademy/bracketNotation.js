6184634894f63d1a2294ba498a24ab08f375ffab
ma3laa <142917357+ma3laa@users.noreply.github.com>
2024-08-22 00:02:04 +0100

    commmitttt

---
+++ codecademy/bracketNotation.js
@@ -0,0 +1,14 @@
+let spaceship = {
+    'Fuel Type' : 'Turbo Fuel',
+    'Active Mission' : true,
+    homePlanet : 'Earth', 
+    numCrew: 5
+   };
+  
+  let propName =  'Active Mission';
+  
+  let isActive = spaceship['Active Mission']
+  
+  console.log(isActive)
+  
+  
